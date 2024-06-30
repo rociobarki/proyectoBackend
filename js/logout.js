bd04fceba3f1ee3d52d1f28393efdcf2639653a1
martinrodriguez <martinrodriguezdicarlo@gmail.com>
2024-06-30 11:41:55 -0300

    cambios3006-2

--- js/logout.js
+++ js/logout.js
@@ -1,13 +1,21 @@
+// const logout = () => {
+//     sessionStorage.removeItem("adm")
+//     window.location.href = './index.html'
+// }
+
+
+// const logoutButton = document.querySelector('#logoutButton')
+// logoutButton.addEventListener('click', logout)
+
 const logout = () => {
     sessionStorage.removeItem("adm")
     window.location.href = './index.html'
 }
 
-
-const logoutButton = document.querySelector('logoutButton')
-logoutButton.addEventListener('click', logout)
-
-
+document.addEventListener('DOMContentLoaded', () => {
+    const logoutButton = document.querySelector('#logoutButton')
+    logoutButton.addEventListener('click', logout)
+})
 
 
 
